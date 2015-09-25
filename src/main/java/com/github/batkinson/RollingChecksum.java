a084b5e5aab6923280ad56d6d19e64d4e4c37bbe
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-25 17:52:05 -0400

    Implemented rolling checksum.

---
+++ src/main/java/com/github/batkinson/RollingChecksum.java
@@ -0,0 +1,68 @@
+package com.github.batkinson;
+
+/**
+ * Implementation of a rolling checksum as described in https://rsync.samba.org/tech_report/node3.html.
+ */
+public class RollingChecksum {
+
+    private static int POW2 = 16;
+    private static int MOD_MASK = (1 << POW2) - 1;
+
+    private int window;
+    private int a;
+    private int b;
+    private int i;
+
+    public RollingChecksum(int window) {
+        this.window = window;
+        reset();
+    }
+
+    public long update(byte prevVal, byte byteVal) {
+        if (i <= window) {
+            update(byteVal);
+        } else {
+            roll(prevVal, byteVal);
+        }
+        return getValue();
+    }
+
+    private void update(byte value) {
+        a = (a + value) & MOD_MASK;
+        b = (b + ((window - i + 1) * value)) & MOD_MASK;
+        i++;
+    }
+
+    private void roll(byte prevStart, byte nextEnd) {
+        a = ((a - prevStart) + nextEnd) & MOD_MASK;
+        b = (b - (window * prevStart) + a) & MOD_MASK;
+    }
+
+    public long start(byte[] content) {
+        return start(content, 0);
+    }
+
+    public long start(byte[] content, int offset) {
+
+        if (i != 1) {
+            throw new IllegalStateException("sum already started");
+        }
+
+        int stopOffset = offset + Math.min(window, content.length - offset);
+        for (int j = offset; j < stopOffset; j++) {
+            update(content[j]);
+        }
+
+        return getValue();
+    }
+
+    public long getValue() {
+        return a + (b << POW2);
+    }
+
+    public void reset() {
+        a = 0;
+        b = 0;
+        i = 1;
+    }
+}
---
+++ src/test/java/com/github/batkinson/RollingChecksumTest.java
@@ -0,0 +1,73 @@
+package com.github.batkinson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RollingChecksumTest {
+
+    byte[] content = "Roll out. Keep rolling, rolling, rolling. Rolling in the deep.".getBytes();
+
+    @Test
+    public void checksumOfOneByte() {
+        RollingChecksum small = new RollingChecksum(1);
+        assertEquals(5374034, small.start(content));
+    }
+
+    @Test
+    public void checksumOfTwoByes() {
+        RollingChecksum small = new RollingChecksum(2);
+        assertEquals(18022593, small.start(content));
+    }
+
+    @Test
+    public void checksumRollsOnce() {
+        RollingChecksum direct = new RollingChecksum(1);
+        RollingChecksum rolled = new RollingChecksum(1);
+        assertEquals(5374034, rolled.start(content));
+        assertEquals(7274607, direct.start(content, 1));
+        assertEquals(7274607, rolled.update(content[0], content[1]));
+    }
+
+    @Test
+    public void checksumRollsMultiple() {
+        RollingChecksum rolled = new RollingChecksum(1);
+        for (int i = 0; i < content.length; i++) {
+            RollingChecksum direct = new RollingChecksum(1);
+            if (i == 0) {
+                assertEquals(direct.start(content), rolled.start(content));
+            } else {
+                assertEquals(direct.start(content, i), rolled.update(content[i - 1], content[i]));
+            }
+        }
+    }
+
+    @Test
+    public void checksumRollsMultipleWithReset() {
+        RollingChecksum rolled = new RollingChecksum(1);
+        RollingChecksum direct = new RollingChecksum(1);
+        for (int i = 0; i < content.length; i++) {
+            direct.reset();
+            if (i == 0) {
+                assertEquals(direct.start(content), rolled.start(content));
+            } else {
+                assertEquals(direct.start(content, i), rolled.update(content[i - 1], content[i]));
+            }
+        }
+    }
+
+    @Test
+    public void testRollsWithLargerWindows() {
+        for (int w = 2; w < content.length; w++) {
+            RollingChecksum rolled = new RollingChecksum(w);
+            for (int i = 0; i < (content.length - w); i++) {
+                RollingChecksum direct = new RollingChecksum(w);
+                if (i == 0) {
+                    assertEquals(direct.start(content), rolled.start(content));
+                } else {
+                    assertEquals(direct.start(content, i), rolled.update(content[i - 1], content[i + w - 1]));
+                }
+            }
+        }
+    }
+}
