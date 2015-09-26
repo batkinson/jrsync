c7c844e97c2e7ea655b6536e0ea11e6b24879792
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-26 18:32:13 -0400

    Added binary files to search tests, factored utilities out.

--- src/test/java/com/github/batkinson/sync/BlockSearchTest.java
+++ src/test/java/com/github/batkinson/sync/BlockSearchTest.java
@@ -9,13 +9,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import static com.github.batkinson.sync.BlockDesc.describe;
+import static com.github.batkinson.sync.TestUtils.computeHash;
+import static com.github.batkinson.sync.TestUtils.fileContent;
 import static com.github.batkinson.sync.TestUtils.openFile;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class BlockSearchTest {
 
@@ -23,11 +25,15 @@ public class BlockSearchTest {
 
     RandomAccessFile basis;
     RandomAccessFile target;
+    RandomAccessFile violin;
+    RandomAccessFile guitar;
 
     @Before
     public void setup() throws URISyntaxException, FileNotFoundException {
         basis = openFile("file1.txt");
         target = openFile("file2.txt");
+        violin = openFile("violin.jpg");
+        guitar = openFile("guitar.jpg");
     }
 
     @After
@@ -41,66 +47,95 @@ public class BlockSearchTest {
         }
     }
 
-    private byte[] fileContent(RandomAccessFile f, long start, long end) throws IOException {
-        long origPos = f.getFilePointer();
-        try {
-            f.seek(start);
-            byte[] bytes = new byte[(int) (end - start)];
-            f.readFully(bytes);
-            return bytes;
-        } finally {
-            f.seek(origPos);
-        }
-    }
-
-    /**
-     * Patches a file together based on block search results. Precursor to actual patch code, but
-     * doesn't handle large content sections.
-     */
-    class FilePatcher implements SearchHandler {
-
-        private int blockSize;
-        private RandomAccessFile dest;
-
-        FilePatcher(String name, int blockSize) throws IOException {
-            this.blockSize = blockSize;
-            this.dest = new RandomAccessFile(File.createTempFile(name, "", null), "rw");
-        }
-
-        public RandomAccessFile getDest() {
-            return dest;
-        }
-
-        @Override
-        public void blockMatch(long offset, long blockIndex) throws IOException {
-            dest.seek(offset);
-            dest.write(fileContent(basis, blockIndex * blockSize, (blockIndex * blockSize) + blockSize));
-        }
-
-        @Override
-        public void needsContent(long startOffset, long endOffset) throws IOException {
-            dest.seek(startOffset);
-            dest.write(fileContent(target, startOffset, endOffset));
-        }
-    }
-
-    /**
-     * Not to be used with large files, allocates the storage in memory.
-     */
-    private byte[] computeHash(RandomAccessFile f) throws IOException, NoSuchAlgorithmException {
-        f.seek(0);
-        byte[] content = new byte[(int)f.length()];
-        f.readFully(content);
-        return MessageDigest.getInstance("SHA1").digest(content);
-    }
-
     @Test
     public void execute() throws IOException, NoSuchAlgorithmException {
         for (int blockSize = 1; blockSize <= target.length(); blockSize++) {
             final BlockSearch search = new BlockSearch(describe(basis, blockSize, MD5), blockSize);
-            FilePatcher patcher = new FilePatcher("executetest", blockSize);
+            FilePatcher patcher = new FilePatcher("bstfile", blockSize, basis, target);
             search.execute(target, MD5, patcher);
             assertArrayEquals(computeHash(target), computeHash(patcher.getDest()));
+            assertEquals(target.length(), patcher.getBytesMatched() + patcher.getBytesNeeded());
         }
+    }
+
+    @Test
+    public void makeViolinIntoGuitar() throws IOException, NoSuchAlgorithmException {
+        for (int blockSize : Arrays.asList(128)) {
+            final BlockSearch search = new BlockSearch(describe(violin, blockSize, MD5), blockSize);
+            FilePatcher patcher = new FilePatcher("bstvig", blockSize, violin, guitar);
+            search.execute(guitar, MD5, patcher);
+            assertArrayEquals(computeHash(guitar), computeHash(patcher.getDest()));
+            assertEquals(guitar.length(), patcher.getBytesMatched() + patcher.getBytesNeeded());
+        }
+    }
+
+    @Test
+    public void makeGuitarIntoViolin() throws IOException, NoSuchAlgorithmException {
+        for (int blockSize : Arrays.asList(128)) {
+            final BlockSearch search = new BlockSearch(describe(guitar, blockSize, MD5), blockSize);
+            FilePatcher patcher = new FilePatcher("bstgiv", blockSize, guitar, violin);
+            search.execute(violin, MD5, patcher);
+            assertArrayEquals(computeHash(violin), computeHash(patcher.getDest()));
+            assertEquals(violin.length(), patcher.getBytesMatched() + patcher.getBytesNeeded());
+        }
+    }
+
+    @Test
+    public void makeGuitarIntoGuitar() throws IOException, NoSuchAlgorithmException {
+        for (int blockSize : Arrays.asList(128)) {
+            final BlockSearch search = new BlockSearch(describe(guitar, blockSize, MD5), blockSize);
+            FilePatcher patcher = new FilePatcher("bstgig", blockSize, guitar, guitar);
+            search.execute(guitar, MD5, patcher);
+            assertArrayEquals(computeHash(guitar), computeHash(patcher.getDest()));
+            assertEquals(guitar.length(), patcher.getBytesMatched() + patcher.getBytesNeeded());
+        }
+    }
+}
+
+/**
+ * Patches a file together based on block search results. Precursor to actual patch code, but
+ * doesn't handle large content sections.
+ */
+class FilePatcher implements SearchHandler {
+
+    private int blockSize;
+    private RandomAccessFile dest;
+    private RandomAccessFile basis;
+    private RandomAccessFile target;
+    private long bytesMatched = 0;
+    private long bytesNeeded = 0;
+
+    FilePatcher(String name, int blockSize, RandomAccessFile basis, RandomAccessFile target) throws IOException {
+        this.blockSize = blockSize;
+        this.dest = new RandomAccessFile(File.createTempFile(name, "", null), "rw");
+        this.basis = basis;
+        this.target = target;
+    }
+
+    public RandomAccessFile getDest() {
+        return dest;
+    }
+
+    @Override
+    public void blockMatch(long offset, long blockIndex) throws IOException {
+        long start = blockIndex * blockSize, end = start + blockSize, size = end - start;
+        bytesMatched += size;
+        dest.seek(offset);
+        dest.write(fileContent(basis, start, end));
+    }
+
+    @Override
+    public void needsContent(long startOffset, long endOffset) throws IOException {
+        bytesNeeded += endOffset - startOffset;
+        dest.seek(startOffset);
+        dest.write(fileContent(target, startOffset, endOffset));
+    }
+
+    public long getBytesMatched() {
+        return bytesMatched;
+    }
+
+    public long getBytesNeeded() {
+        return bytesNeeded;
     }
 }
--- src/test/java/com/github/batkinson/sync/TestUtils.java
+++ src/test/java/com/github/batkinson/sync/TestUtils.java
@@ -2,16 +2,49 @@ package com.github.batkinson.sync;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class TestUtils {
 
+    /**
+     * Opens the named file from the first match in same package as this class. Used for test files.
+     */
     public static RandomAccessFile openFile(String path) throws URISyntaxException, FileNotFoundException {
         URL url = BlockDescTest.class.getResource(path);
         File f = new File(url.toURI());
         return new RandomAccessFile(f, "r");
     }
+
+    /**
+     * Gets the specified section of the file as a byte array, then returns the file to its previous
+     * position.
+     */
+    public static byte[] fileContent(RandomAccessFile f, long start, long end) throws IOException {
+        long origPos = f.getFilePointer();
+        try {
+            f.seek(start);
+            byte[] bytes = new byte[(int) (end - start)];
+            f.readFully(bytes);
+            return bytes;
+        } finally {
+            f.seek(origPos);
+        }
+    }
+
+    /**
+     * Not to be used with large files, allocates the storage in memory.
+     */
+    public static byte[] computeHash(RandomAccessFile f) throws IOException, NoSuchAlgorithmException {
+        f.seek(0);
+        byte[] content = new byte[(int)f.length()];
+        f.readFully(content);
+        return MessageDigest.getInstance("SHA1").digest(content);
+    }
+
 
 }
