f1d273f8ed98cd75465b652c49ff5f5a3f5d4d4f
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-26 17:41:48 -0400

    Made search handler interface public and added brief doc.

--- src/main/java/com/github/batkinson/sync/SearchHandler.java
+++ src/main/java/com/github/batkinson/sync/SearchHandler.java
@@ -2,7 +2,12 @@ package com.github.batkinson.sync;
 
 import java.io.IOException;
 
-interface SearchHandler {
+/**
+ * The contract for handling block search results. The methods should always be called in the byte
+ * order of the target file (the one being searched), which can be important for stream-based ops
+ * such as compression, etc.
+ */
+public interface SearchHandler {
     void blockMatch(long startOffset, long blockIndex) throws IOException;
     void needsContent(long startOffset, long endOffset) throws IOException;
 }
