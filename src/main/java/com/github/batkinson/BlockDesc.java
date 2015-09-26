0eb9809939eddbb2f2163952a812cd95a1a2a097
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-25 23:39:52 -0400

    Added block descriptors and method of splitting files into them.

---
+++ src/main/java/com/github/batkinson/BlockDesc.java
@@ -0,0 +1,40 @@
+package com.github.batkinson;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Describes blocks from the byte stream we wish to use as a copy basis.
+ */
+class BlockDesc {
+
+    long blockIndex;
+    long weakChecksum;
+    byte[] cryptoHash;
+
+    public BlockDesc(long blockIndex, long weakChecksum, byte[] cryptoHash) {
+        this.blockIndex = blockIndex;
+        this.weakChecksum = weakChecksum;
+        this.cryptoHash = cryptoHash;
+    }
+
+    public static List<BlockDesc> describe(RandomAccessFile file, int blockSize, String digestAlgorithm) throws IOException, NoSuchAlgorithmException {
+        List<BlockDesc> blockDescs = new ArrayList<>();
+        long length = file.length();
+        byte[] block = new byte[blockSize];
+        MessageDigest digest = MessageDigest.getInstance(digestAlgorithm);
+        RollingChecksum checksum = new RollingChecksum(blockSize);
+        for (int i = 0, blockStart = 0; blockStart + blockSize <= length; i++, blockStart += blockSize) {
+            file.seek(blockStart);
+            file.readFully(block);
+            blockDescs.add(new BlockDesc(i, checksum.start(block), digest.digest(block)));
+            checksum.reset();
+        }
+        return blockDescs;
+    }
+
+}
---
+++ src/test/java/com/github/batkinson/BlockDescTest.java
@@ -0,0 +1,74 @@
+package com.github.batkinson;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class BlockDescTest {
+
+    private static final Logger log = LoggerFactory.getLogger(BlockDescTest.class);
+
+    private static final String MD5 = "MD5";
+
+    private RandomAccessFile input;
+
+    @Before
+    public void setup() throws URISyntaxException, FileNotFoundException {
+        URL url = BlockDescTest.class.getResource("file1.txt");
+        File f = new File(url.toURI());
+        input = new RandomAccessFile(f, "r");
+    }
+
+    @After
+    public void teardown() throws IOException {
+        input.close();
+        input = null;
+    }
+
+    @Test
+    public void describe() throws IOException, NoSuchAlgorithmException {
+        long fileLength = input.length();
+        for (int blockSize = 1; blockSize <= fileLength; blockSize++) {
+            int readBytes = 0;
+            RollingChecksum checksum = new RollingChecksum(blockSize);
+            MessageDigest digest = MessageDigest.getInstance(MD5);
+            byte[] block = new byte[blockSize];
+            List<BlockDesc> descs = BlockDesc.describe(input, blockSize, MD5);
+            for (BlockDesc d : descs) {
+                input.seek(d.blockIndex * blockSize);
+                input.readFully(block);
+                readBytes += blockSize;
+                if (log.isDebugEnabled()) {
+                    log.debug(String.format("'%s' %d: %s %s%n", new String(block), d.blockIndex, d.weakChecksum, toHex(d.cryptoHash)));
+                }
+                checksum.reset();
+                assertEquals(checksum.start(block), d.weakChecksum);
+                assertArrayEquals(digest.digest(block), d.cryptoHash);
+            }
+            assertEquals(readBytes, descs.size() * blockSize);
+            assertEquals(fileLength % blockSize, fileLength - readBytes);
+        }
+    }
+
+    private String toHex(byte[] bytes) {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++)
+            b.append(String.format("%02x", bytes[i]));
+        return b.toString();
+    }
+}
