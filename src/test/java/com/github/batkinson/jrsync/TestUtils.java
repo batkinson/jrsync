b54193825b89c042e69138596db6edc99ba2d20f
Brent Atkinson <brent.atkinson@gmail.com>
2015-10-08 13:20:37 -0400

    Added test utilities for describing files by checksums/content hashes

--- src/test/java/com/github/batkinson/jrsync/TestUtils.java
+++ src/test/java/com/github/batkinson/jrsync/TestUtils.java
@@ -63,4 +63,60 @@ public class TestUtils {
             f.seek(origPos);
         }
     }
+
+    /**
+     * Used to print contents of a byte array in hexadecimal.
+     */
+    public static String toHex(byte[] bytes) {
+        String[] hexDigit = {
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"
+        };
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            sb.append(hexDigit[(b & 0xF0) >>> 4]);
+            sb.append(hexDigit[b & 0x0F]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Converts an int value into a byte array.
+     */
+    public static byte[] toBytes(int v) {
+        byte[] result = new byte[4];
+        for (int i = 0; i < result.length; i++) {
+            int byteShift = result.length - 1 - i, bitShiftAmt = byteShift << 3;
+            int mask = 0xFF << bitShiftAmt;
+            result[i] = (byte) ((v & mask) >>> bitShiftAmt);
+        }
+        return result;
+    }
+
+    /**
+     * Prints a description of the file, useful for constructing metadata.
+     */
+    public static void describeFile(RandomAccessFile file, int blockSize, String fileAlg, String blockAlg) throws IOException, URISyntaxException, NoSuchAlgorithmException {
+        byte[] content = new byte[blockSize];
+        RollingChecksum sum = new RollingChecksum(blockSize);
+        MessageDigest fileDigest = MessageDigest.getInstance(fileAlg);
+        long blocks = file.length() / blockSize,
+                remainder = file.length() % blockSize;
+        for (int i = 0; i < blocks; i++) {
+            MessageDigest blockDigest = MessageDigest.getInstance(blockAlg);
+            file.readFully(content);
+            sum.update(content);
+            fileDigest.update(content);
+            System.out.printf("block %d: sum: %s, digest: %s%n",
+                    i,
+                    toHex(toBytes((int) sum.getValue())),
+                    toHex(blockDigest.digest(content)));
+        }
+        if (remainder > 0) {
+            file.readFully(content, 0, (int) remainder);
+            fileDigest.update(content, 0, (int) remainder);
+            System.out.printf("block ~: %d addt'l bytes%n", remainder);
+        }
+        System.out.printf("file: %d bytes, digest: %s%n", file.length(), toHex(fileDigest.digest()));
+    }
 }
