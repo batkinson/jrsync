56f3ccb80812d2d7d2f2fd69a1556a347ee55c67
Brent Atkinson <brent.atkinson@gmail.com>
2015-10-11 17:40:56 -0400

    Removed unused imports.

--- src/main/java/com/github/batkinson/jrsync/BlockDesc.java
+++ src/main/java/com/github/batkinson/jrsync/BlockDesc.java
@@ -1,12 +1,5 @@
 package com.github.batkinson.jrsync;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Describes blocks from the byte stream we wish to use as a copy basis.
  */
